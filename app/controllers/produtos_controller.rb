1ca1a1037943947cb8dc60813cfead22782d1db8
Mauricio Linhares <mauricio.linhares@gmail.com>
2012-07-02 20:18:26 -0300

    flash

--- app/controllers/produtos_controller.rb
+++ app/controllers/produtos_controller.rb
@@ -16,8 +16,11 @@ class ProdutosController < ApplicationController
   def create
     @produto = Produto.new(params[:produto])
     if @produto.save
+      flash[:success] = 'O produto foi salvo com sucesso'
       redirect_to produtos_path
     else
+      flash.now[:error] =
+          'Ocorreram erros na hora de gravar o produto'
       render :new
     end
   end

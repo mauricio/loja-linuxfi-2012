6e0f52ca920862ab372093e0e9fe951a5b28d89d
Mauricio Linhares <mauricio.linhares@gmail.com>
2012-07-04 19:26:20 -0300

    index

--- app/models/pedido.rb
+++ app/models/pedido.rb
@@ -3,7 +3,6 @@ class Pedido < ActiveRecord::Base
   has_many :itens
 
   def adicionar_produto( produto, quantidade )
-
     if item = self.itens.detect { |i| i.produto == produto }
       item.incrementar_quantidade(quantidade)
       item.save!
@@ -12,7 +11,5 @@ class Pedido < ActiveRecord::Base
           :produto => produto,
           :quantidade => quantidade)
     end
-
   end
-
 end

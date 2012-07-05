c28b7e1e6c63d5f32b0d529f6d2b177fe0583fca
Mauricio Linhares <mauricio.linhares@gmail.com>
2012-07-04 21:03:46 -0300

    columns com default

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+
+  def render_head(clazz, * columns)
+
+    content_tag :thead do
+      content_tag :tr do
+        table_headers = columns.map do |column|
+          content_tag(
+              :th,
+              clazz.human_attribute_name(column))
+        end
+
+        table_headers.join("\n").html_safe
+      end
+    end
+
+  end
+
 end
